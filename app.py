adbb8d9567922ed06f27d2b30b4653db911c9e7e
RentoSaijo <rentosaijo0527@gmail.com>
2025-12-23 19:43:26 +0900

    Add assets

--- app.py
+++ app.py
@@ -1,9 +1,3 @@
 import streamlit as st
-from streamlit.components.v1 import html
-    html('''
-       <script>
-        window.top.document.querySelectorAll(`[href*="streamlit.io"]`).forEach(e => e.setAttribute("style", "display: none;"));
-      </script>
-    ''')
-
+st.logo(image = "assets/Letter.png", size = "large")
 st.title("Rento's Rink")
