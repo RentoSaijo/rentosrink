47afeb26ea7eb24dcd3b5d12b600610b8b9a32ef
RentoSaijo <rentosaijo0527@gmail.com>
2025-12-24 14:29:17 +0900

    Test scatter plot

--- app.py
+++ app.py
@@ -1,3 +1,22 @@
+# Load libraries.
 import streamlit as st
-st.logo(image = "assets/Letter.png", size = "large")
-st.title("Rento's Rink")
+import pandas as pd
+
+# Logo
+st.logo(image = 'assets/Letter.png', size = 'large')
+
+# Title
+st.title('Rento\'s Rink')
+
+# Load data.
+@st.cache_data
+def load_skaters_xG():
+    return pd.read_csv('data/skaters_xG.csv')
+skaters_xG = load_skaters_xG()
+
+# Create scatter plot.
+st.scatter_chart(
+    skaters_xG,
+    x = "ixGF_20242025_2",
+    y = "iGF_20242025_2",
+)
